2959b543d045cbbbb089e7483284e8c61e36602b
MistbornL <lashaloria3@gmail.com>
2022-12-07 23:52:10 +0400

    canvas

--- src/App.tsx
+++ src/App.tsx
@@ -10,13 +10,19 @@ function App() {
     context.strokeRect(100, 120, 50, 50);
   };
 
-  const drawExampke = (context: CanvasRenderingContext2D) => {};
+  const drawExample = (context: CanvasRenderingContext2D) => {
+    context.fillStyle = "cornflowerblue";
+    context.fillRect(20, 20, 100, 120);
+
+    context.fillStyle = "cornflowerblue";
+    context.strokeRect(90, 95, 74, 75);
+  };
   return (
     <div className="App">
       <h1>example</h1>
       <Canvas width={window.innerWidth} height={400} draw={drawArt} />
       <h1>art</h1>
-      <Canvas width={window.innerWidth} height={400} draw={drawArt} />
+      <Canvas width={window.innerWidth} height={400} draw={drawExample} />
     </div>
   );
 }
