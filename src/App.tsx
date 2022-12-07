0fab91a9a6adb2d3dd3992e70fbdc6fbde9f51c2
MistbornL <lashaloria3@gmail.com>
2022-12-07 23:47:28 +0400

    canvas

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,19 @@ import "./App.css";
 import { Canvas } from "./Canvas";
 
 function App() {
+  const drawArt = (context: CanvasRenderingContext2D) => {
+    context.fillStyle = "cornflowerblue";
+    context.fillRect(0, 0, 100, 120);
+    context.strokeRect(100, 120, 50, 50);
+  };
+
+  const drawExampke = (context: CanvasRenderingContext2D) => {};
   return (
     <div className="App">
-      <Canvas />
+      <h1>example</h1>
+      <Canvas width={window.innerWidth} height={400} draw={drawArt} />
+      <h1>art</h1>
+      <Canvas width={window.innerWidth} height={400} draw={drawArt} />
     </div>
   );
 }
--- src/Canvas.tsx
+++ src/Canvas.tsx
@@ -1,12 +1,23 @@
 import React, { useEffect, useRef } from "react";
+type CanvasProps = React.DetailedHTMLProps<
+  React.CanvasHTMLAttributes<HTMLCanvasElement>,
+  HTMLCanvasElement
+> & { draw: (context: CanvasRenderingContext2D) => void };
 
-export const Canvas = () => {
+export const Canvas: React.FC<CanvasProps> = ({ draw, ...props }) => {
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
 
   useEffect(() => {
     const canvas = canvasRef.current;
-    // if()
-  }, []);
+    if (!canvas) {
+      return;
+    }
+    const context = canvas.getContext("2d");
+    if (!context) {
+      return;
+    }
+    draw(context);
+  }, [draw]);
 
-  return <canvas ref={canvasRef} />;
+  return <canvas width={props.width} height={props.height} ref={canvasRef} />;
 };
